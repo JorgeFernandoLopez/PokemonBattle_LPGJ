3b937449efe847edf8cd998a967c2872d0281203
Jorge Fernando Lopez <jorgefernandolope117@gmail.com>
2026-07-01 20:28:42 -0600

    PokemonBattle_LPGJ_01/07/2026

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,38 +2,49 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using System.Collections;
-
+ 
 public class Timer : MonoBehaviour
 {
     [SerializeField]
-    private TimerData[]timerData;
-     [SerializeField]
+    private TimerData[] timerData;
+    [SerializeField]
     private Image timerImage;
-     [SerializeField]
-     private UnityEvent onTimerEnd;
+    private Animator animator;
+    [SerializeField]
+    private UnityEvent onTimerEnd;
+    private void Awake()
+    {
+        animator = timerImage.GetComponent<Animator>();
+    }
     public void StartTimer(int duration)
     {
-        StartCoroutine(TimerCoroutine( duration));
+        StartCoroutine(TimerCoroutine(duration));
     }
     private IEnumerator TimerCoroutine(int duration)
     {
         while (duration > 0)
         {
-                SoundManager.instance.Play(timerData[duration - 1].soundName);
-                timerImage.sprite=timerData[duration - 1].texture;
-                yield return new WaitForSeconds(1f);
-                duration --;
+            SoundManager.instance.Play(timerData[duration - 1].soundName);
+            timerImage.sprite = timerData[duration - 1].texture;
+            timerImage.SetNativeSize();
+            animator.Play("Show", 0, 0f);
+            yield return new WaitForSeconds(1f);
+            duration--;
         }
         onTimerEnd.Invoke();
     }
     public void StopTimer()
     {
+        animator.Play("Hidden", 0, 0f);
         StopAllCoroutines();
     }
 }
+ 
 [System.Serializable]
 public class TimerData
 {
     public string soundName;
     public Sprite texture;
 }
+ 
+ 
