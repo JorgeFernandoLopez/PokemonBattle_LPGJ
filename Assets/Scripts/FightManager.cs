aaad2e4960132cadf44ac5247f3305ac3deaf08c
Jorge Fernando Lopez <jorgefernandolope117@gmail.com>
2026-07-06 20:47:50 -0600

    POKEMONBATTLE_LPGJ_06/07/2026

--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -11,6 +11,10 @@ public class FightManager : MonoBehaviour
     private UnityEvent onCancelFight;
     [SerializeField]
     private UnityEvent onFightStart;
+    [SerializeField]
+    private UnityEvent <string> onFightEnd;
+    [SerializeField]
+    private UnityEvent<DamageTarget> onDamageTaken;
      [SerializeField]
     private int minimumFighters=2;
      [SerializeField]
@@ -18,6 +22,7 @@ public class FightManager : MonoBehaviour
        [SerializeField]
        private PoolManager poolManager;
     private List<Fighter> fighters =new List<Fighter>();
+    private DamageTarget damageTarget = new DamageTarget();
     public void AddFighter(Fighter fighter)
     {
         if (fighters.Count < maximunFighters && !fighters.Contains(fighter))
@@ -25,6 +30,7 @@ public class FightManager : MonoBehaviour
             poolManager.GetObject(fighter.FighterData.appearParticles, fighter.transform.position);
             SoundManager.instance.Play(fighter.FighterData.appearSoundName);
             fighters.Add(fighter);
+            DialogSystem.Instance.ShowDialog(fighter.FighterData.fighterName + "has joined the fight!");
             if(fighters.Count >= minimumFighters)
             {
                 onFightReady?.Invoke();
@@ -66,6 +72,7 @@ public class FightManager : MonoBehaviour
              defender.transform.LookAt(attacker.transform);
             attacker.Animator.Play("Charge",0,0f);
             yield return new WaitForSeconds(attacker.FighterData.chargeTime);
+            DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName+"attacks with" + attackData.name + "!");
              poolManager.GetObject(attackData.chargeParticles, attacker.transform.position);
             attacker.Animator.Play(attackData.animationName, 0, 0f);
              SoundManager.instance.Play(attackData.attakcSoundName);
@@ -74,11 +81,16 @@ public class FightManager : MonoBehaviour
             poolManager.GetObject(attackData.attackParticles, defender.transform.position);
             Health defenderHealth = defender.GetComponent<Health>();
             SoundManager.instance.Play(defender.FighterData.damageSoundName);
+            float damage = Random.Range(attackData.minDamage, attackData.maxDamage);
+            damageTarget.SetDamageTarget(defender.transform,damage);
+            defenderHealth.TakeDamage(damage);
+            onDamageTaken?.Invoke(damageTarget);
             defenderHealth.TakeDamage(Random.Range(attackData.minDamage, attackData.maxDamage));
             if (defenderHealth.CurrentHealth <= 0)
             {
                 
                 SoundManager.instance.Play(defender.FighterData.deadSoundName);
+                DialogSystem.Instance.ShowDialog(attacker.FighterData.fighterName + "wins the fiht!");
                 RemoveFighter(defender);
                 FighterWin(attacker);
             }
@@ -87,6 +99,8 @@ public class FightManager : MonoBehaviour
     }
     private void FighterWin(Fighter winner)
     {
-        Debug.Log(winner.name+"Wins the fight!");
+      onFightEnd?.Invoke(winner.FighterData.fighterName);
+      winner.Animator.Play("Victory",0,0f);
+      winner.transform.LookAt(Camera.main.transform);
     }
 }
